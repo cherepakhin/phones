807fa9bc2bd92cdb7799d62daade120d3b244341
vasi <vasi.che@gmail.com>
2022-09-10 17:40:09 +0500

    Пляски с модулем dto закончены

--- commutator/src/main/java/ru/perm/v/phones/commutator/CommutatorApplication.java
+++ commutator/src/main/java/ru/perm/v/phones/commutator/CommutatorApplication.java
@@ -1,7 +1,9 @@
 package ru.perm.v.phones.commutator;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class CommutatorApplication {
     public static void main(String[] args) {
         SpringApplication.run(CommutatorApplication.class, args);
