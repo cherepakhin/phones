72ae5949a3aaa515cc7d97f75e109bd8b66e0012
vasi <vasi.che@gmail.com>
2022-09-10 14:21:14 +0500

    Добавлены BillingApplication, CommutatorApplication, application.yaml. Подкорректированы application.yaml модулей

---
+++ billing/src/main/java/ru/perm/v/phones/billing/BillingApplication.java
@@ -0,0 +1,9 @@
+package ru.perm.v.phones.billing;
+
+import org.springframework.boot.SpringApplication;
+
+public class BillingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BillingApplication.class, args);
+    }
+}
---
+++ commutator/src/main/java/ru/perm/v/phones/commutator/CommutatorApplication.java
@@ -0,0 +1,9 @@
+package ru.perm.v.phones.commutator;
+
+import org.springframework.boot.SpringApplication;
+
+public class CommutatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommutatorApplication.class, args);
+    }
+}
